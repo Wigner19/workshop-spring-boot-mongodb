0bec656002f625b7794cf5ba48eff96536b79ddf
Wigner_Santos <61386050+Wigner19@users.noreply.github.com>
2021-05-11 14:55:41 -0300

    Referenciando os posts do usuario

--- src/main/java/com/wignersantos/workshopmongo/domain/User.java
+++ src/main/java/com/wignersantos/workshopmongo/domain/User.java
@@ -1,8 +1,11 @@
 package com.wignersantos.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document
@@ -15,6 +18,9 @@ public class User implements Serializable {
 	private String name;
 	private String email;
 	private String phone;
+	
+	@DBRef(lazy = true)
+	private List<Post> posts = new ArrayList<>();
 	
 	public User() {
 	}
@@ -57,6 +63,14 @@ public class User implements Serializable {
 	
 	public void setPhone(String phone) {
 		this.phone = phone;
+	}
+	
+	public List<Post> getPosts() {
+		return posts;
+	}
+	
+	public void setPosts(List<Post> posts) {
+		this.posts = posts;
 	}
 
 	@Override
